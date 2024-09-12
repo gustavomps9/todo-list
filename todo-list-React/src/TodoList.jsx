8e6fe20b3ae0239c18e93b5da75e5d0e289c1ae9
gustavo <gustavo.silva9@ua.pt>
2024-09-12 15:02:53 +0100

    Todo List com React.
    Diversas funcionalidade tais como:
    Adicionar Tarefa;
    Remover Tarefa;
    Mudar prioridade da tarefa (Mover para cima e para baixo)

---
+++ todo-list-React/src/TodoList.jsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+
+function TodoList(){
+   const [tasks, setTasks] = useState(["Tarefa 1", "Tarefa 2", "Tarefa 3"]);
+   const [newTask, setNewTask] = useState("");
+
+   function handleInputChange(event){
+      setNewTask(event.target.value);
+   }
+
+   function addTask(){
+      if(newTask.trim() !== ""){
+         setTasks([...tasks, newTask]);
+         setNewTask("");
+      }
+   }
+
+   function deleteTask(index){
+      const deleteTasks = tasks.filter((_, i) => i !== index);
+      setTasks(deleteTasks);
+   }
+   
+   function moveUp(index){
+      if(index > 0){
+         const newTasks = [...tasks];
+         const aux = newTasks[index];
+         newTasks[index] = newTasks[index - 1];
+         newTasks[index - 1] = aux;
+         setTasks(newTasks);
+
+      }
+   }
+
+   function moveDown(index){
+      if(index < tasks.length - 1){
+         const newTasks = [...tasks];
+         const aux = newTasks[index];
+         newTasks[index] = newTasks[index + 1];
+         newTasks[index + 1] = aux;
+         setTasks(newTasks);
+      }
+   }
+
+   return(
+      <div className='todo-list'>
+         <h1>To-Do-List</h1>
+         <div>
+            <input type='text' value={newTask} onChange={handleInputChange} placeholder='Insira uma tarefa...'/>
+            <button className="add-btn" onClick={addTask}>Adicionar</button>
+         </div>
+
+         <ol>
+            {tasks.map((task, index) => (
+               <li key={index}>
+                  <span className="task">{task}</span>
+                  <div className='buttons'>
+                  <button className="delete-btn" onClick={() => deleteTask(index)}>Remover</button>
+                  <button className="up-btn" onClick={() => moveUp(index)}>⬆️</button>
+                  <button className="down-btn" onClick={() => moveDown(index)}>⬇️</button>
+                  </div>
+               </li>
+            ))}
+         </ol>
+      </div>
+   );
+}
+
+export default TodoList;
